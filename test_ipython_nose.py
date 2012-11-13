a76f004996ed06f9610f71e5f48cae7ba23fd516
Taavi Burns <taavi@taaviburns.ca>
2012-11-13 17:02:03 -0500

    Adds more tests.

--- test_ipython_nose.py
+++ test_ipython_nose.py
@@ -14,17 +14,38 @@ def assert_not_in(subject, container):
 
 
 class TestMyResult(object):
+    def setup(self):
+        self.result = ipython_nose.MyResult()
+
+    def test_summary_says_num_passed_and_total(self):
+        summary = self.result._summary(numtests=5, numfailed=3)
+        assert_in('2/5 tests passed', summary)
+
     def test_summary_with_0_failed_doesnt_say_failed(self):
-        r = ipython_nose.MyResult()
-        summary = r._summary(1, 0)
+        summary = self.result._summary(numtests=1, numfailed=0)
         assert_not_in('failed', summary)
 
     def test_summary_with_1_failed_does_say_failed(self):
-        r = ipython_nose.MyResult()
-        summary = r._summary(1, 1)
-        assert_in('failed', summary)
+        summary = self.result._summary(numtests=1, numfailed=1)
+        assert_in(' 1 failed', summary)
+
+    def test_summary_with_0_failed_tests_has_0_and_100_bars(self):
+        summary = self.result._summary(numtests=1, numfailed=0)
+        assert_in(' 0%', summary)
+        assert_in(' 100%', summary)
+
+    def test_summary_with_1_of_1000_passed_tests_has_5_and_95_bars(self):
+        # numfailed goes from 0 to 5, so you can always see it clearly
+        summary = self.result._summary(numtests=1000, numfailed=999)
+        assert_in(' 1%', summary)
+        assert_in(' 99%', summary)
+
+    def test_summary_with_999_of_1000_passed_tests_has_1_and_99_bars(self):
+        # numfailed is truncated to 99 so you can always see a sliver of hope
+        summary = self.result._summary(numtests=1000, numfailed=1)
+        assert_in(' 5%', summary)
+        assert_in(' 95%', summary)
 
     def test_repr_html_works_with_no_tests(self):
-        r = ipython_nose.MyResult()
-        r.testsRun = 0
-        eq_('No tests found.', r._repr_html_())
+        self.result.testsRun = 0
+        eq_('No tests found.', self.result._repr_html_())
